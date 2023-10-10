35cfb5b7dbfb81b079b6d18e0d56f0683959fd73
Simon KERN <skern@kth.se>
2023-10-10 16:19:34 +0200

    CGL parameters and testing for spectra

---
+++ CGL_parameters.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+def CGL(mu0,cu = 0.1, cd = -1, U = 2, verb = False):
+
+    mu    = mu0 - cu**2
+    nu    = U + 2*1j*cu
+    gamma = 1.0 + 1j*cd
+
+    Umax  = U + 2*cu*cd
+    mu_t = Umax**2/(4*abs(gamma)**2) # - 2*cu**2
+
+    if verb:
+        print(f'Complex Ginzburg-Landau equations for parallel flows:\n')
+        print(f'  U     = {U:.2f}')
+        print(f'  cu    = {cu:.2f}')
+        print(f'  cd    = {cd:.2f}')
+        print(f'  mu_0  = {mu0:.2f}\n')
+        print(f'  mu    = {mu:.2f}')
+        print(f'  nu    = {nu:.2f}')
+        print(f'  gamma = {gamma:.2f}\n')
+        print(f'  Group velocity:\n    Umax  = {Umax:.2f}')
+        print(f'  Threshold local absolute instability:  mu_0 > mu_t\n    mu_t = {mu_t:.6f}\n')
+
+    return mu,nu,gamma,Umax,mu_t
+
+def CGL2(x, mu0, cu = 0.1, cd = -1, U = 2, mu2 = -0.01, verb = False):
+
+    mu_scal,nu,gamma,Umax,mu_t = CGL(mu0,cu,cd,U)
+
+    mu = mu_scal*np.ones(x.shape) + mu2*x**2/2
+
+    h = np.sqrt(-2*mu2*gamma)
+    mu_c = mu_t + np.abs(h)/2*np.cos(np.angle(gamma)/2)
+
+    if verb:
+        print(f'Complex Ginzburg-Landau equations for spatially developing flows:\n')
+        print(f'  U     = {U:.2f}')
+        print(f'  cu    = {cu:.2f}')
+        print(f'  cd    = {cd:.2f}')
+        print(f'  mu_0  = {mu0:.2f}\n')
+        print(f'  mu    = {mu_scal:.2f} + {mu2:.2f}*x**2/2')
+        print(f'  nu    = {nu:.2f}')
+        print(f'  gamma = {gamma:.2f}\n')
+        print(f'  Group velocity:\n    Umax  = {Umax:.2f}')
+        print(f'  Threshold local absolute instability:  mu_0 > mu_t')
+        print(f'    mu_t = {mu_t:.6f}')
+        print(f'  Threshold global instability:          mu_0 > mu_c')
+        print(f'    mu_c = {mu_c:.6f}\n')
+        if np.max(mu) > 0:
+            x12 = np.sqrt(-2*mu_scal/mu2)
+            print(f'  Region of instability:')
+            print(f'    xI,xII = +/- {x12:.2f}\n')
+
+    return mu,nu,gamma,Umax,mu_t
---
+++ CGL_spectrum.py
@@ -0,0 +1,87 @@
+import numpy as np
+from scipy import linalg as LA
+
+from git.CGL_parameters import *
+from git.diff_mat import *
+from matplotlib import pyplot as plt
+
+# Parameters
+x0 = -30                       # beginning of spatial domain
+x1 = 30                         # end of spatial domain
+dx = 0.1                      # Spatial discretisation
+dt = 0.01                       # Time step
+T  = 20                         # Total simulation time
+
+# Discretisation grids
+L  = x1-x0                      # spatial domain size
+Nx = int(L / dx)                # number of spatial dof
+Nt = int(T / dt)                # number of timesteps
+x = np.linspace(x0, x1, Nx)     # spatial grid
+t = np.linspace(0, T, Nt)       # temporal grid
+
+# Parameters of the complex Ginzburg-Landau equation
+# basic
+U   = 2              # convection speed
+cu  = 0.1
+cd  = -1
+mu0 = 0.38
+mu2 = -0.01
+
+mu,nu,gamma,Umax,mu_t = CGL2(x,mu0,cu,cd,U,mu2,True)
+
+h = np.sqrt(-2*mu2*gamma)
+mu_c = mu_t + np.abs(h)/2*np.cos(np.angle(gamma)/2)
+
+neigs = 100
+l = np.zeros((neigs,),dtype='complex')
+
+for n in range(neigs):
+    l[n] = (mu0 - cu**2) - (nu**2/(4*gamma)) - (n + 0.5)*h
+
+# Generate differentiation matrices
+DM1f,DM1b,DM2c = FDmat(x)
+
+L  = np.matrix(np.diag(mu) - nu*DM1f + gamma*DM2c)
+Lb = np.matrix(np.diag(mu) - nu*DM1b + gamma*DM2c)
+
+d,  v  = LA.eig(L)
+dh, vh = LA.eig(Lb.H)
+
+idx = np.argsort(-np.real(d))
+d   = d[idx]
+v   = v[:,idx]
+
+idx = np.argsort(-np.real(dh))
+dh  = dh[idx]
+vh  = vh[:,idx]
+
+plt.figure()
+plt.axhline(y=0,color='k')
+plt.axvline(x=0,color='k')
+plt.scatter( np.imag(l),np.real(l),s=40,facecolors='none',edgecolors='k',label='analytical')
+plt.scatter(-np.imag(l),np.real(l),s=40,facecolors='none',edgecolors='k')
+plt.scatter( np.imag(d),np.real(d),s=20,color='r',label='FD')
+plt.scatter( np.imag(dh),np.real(dh),s=20,color='r')
+plt.scatter(np.imag(d[0]),np.real(d[0]),s=60,marker='x')
+plt.xlabel('Re')
+plt.ylabel('Im')
+plt.xlim([-2,2])
+plt.ylim([-6,2])
+plt.legend()
+
+plt.figure(figsize = (10,6))
+plt.subplot(1,2,1)
+plt.axhline(y=0,color='k')
+plt.plot(x,np.real(v[:,0]),'r', label='Re')
+plt.plot(x,np.imag(v[:,0]),'b', label='Im')
+plt.plot(x,np.abs(v[:,0]),'k' , label='|.|')
+plt.plot(x,-np.abs(v[:,0]),'k')
+plt.subplot(1,2,2)
+plt.axhline(y=0,color='k')
+plt.plot(x,np.real(vh[:,0]),'r', label='Re')
+plt.plot(x,np.imag(vh[:,0]),'b', label='Im')
+plt.plot(x,np.abs(vh[:,0]),'k' , label='|.|')
+plt.plot(x,-np.abs(vh[:,0]),'k')
+plt.legend()
+
+plt.show()
