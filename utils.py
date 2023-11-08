a9301c980f4e5102cf4afd20f007b2a6b4844b5d
Simon Kern <skern@kth.se>
2023-11-08 14:15:19 +0100

    Updated utils with printing routines for mat and vec

--- utils.py
+++ utils.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 import numpy as np
 
 def enormab(a,b):
@@ -9,7 +7,7 @@ def enormab(a,b):
     <a,b> = b^H a
     
     """
-    return np.dot(b.conj(),a)
+    return np.dot(b.conj().T,a)
 
 
 def enorm(a):
@@ -26,3 +24,42 @@ def en(a):
     Compute vector norm from energy inner product
     """
     return np.sqrt(enorm(a))
+
+def pmat(a):
+    for row in a:
+        for col in row:
+            print("{:8.3f}".format(col), end=" ")
+        print("")
+
+def pvec(v):
+    l = v.size
+    lp = 2
+    lmax = 2*lp
+    if l > lmax:
+        a = v[:lp]
+        b = v[-lp:]
+        split = True
+    else:
+        a = v
+        split = False
+    
+    if any(np.iscomplex(a)):
+        for i, data in enumerate(a):
+            dre = np.real(data)
+            dim = np.imag(data)
+            print(f'\t{i+1:3d}: {dre:8.3f} + {dim:8.3f} i')
+        if split:
+            print('            ...')
+            for i, data in enumerate(b):
+                dre = np.real(data)
+                dim = np.imag(data)
+                print(f'\t{l-lp+i+1:3d}: {dre:8.3f} + {dim:8.3f} i')
+    else:
+        for i, data in enumerate(a):
+            print(f'\t{i+1:3d}: {np.real(data):8.3f}')
+        if split: 
+            print('            ...')
+            for i, data in enumerate(b):
+                dre = np.real(data)
+                dim = np.imag(data)
+                print(f'\t{l-lp+i+1:3d}: {dre:8.3f} + {dim:8.3f} i')
